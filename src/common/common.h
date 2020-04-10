cbbc8b57ab0b643c28086d607fdd27c9431a1bb9
Ofri Snir <ofri.snir@gmail.com>
2020-04-10 13:36:32 -0700

    Add debug print macros to common header.
    Added DEBUG_PRINT and DEBUG_PRINT_WTITH_ERRNO macro to the common
    header file, which allow printing debug messages with nice formatting.

--- src/common/common.h
+++ src/common/common.h
@@ -1,6 +1,7 @@
 
 #include <stdio.h>
-
+#include <errno.h>
+#include <string.h>
 
 /*** Constants ***/
 
@@ -8,4 +9,17 @@
 #define C_FAILURE (-1)
 #define C_SUCCESS (0)
 
+#ifdef DEBUG
+    #define DEBUG_PRINT(fmt, args...) fprintf(stderr, "DEBUG: %s:%d:%s(): " fmt "\n", \
+        __FILE__, __LINE__, __func__, ##args)
+#else
+    #define DEBUG_PRINT(fmt, args...) /* Don't do anything in release builds */
+#endif
+
+#ifdef DEBUG
+#define DEBUG_PRINT_WITH_ERRNO(fmt, args...) fprintf(stderr, "DEBUG: %s:%d:%s(): " fmt ":%s\n", \
+        __FILE__, __LINE__, __func__, ##args, strerror(errno))
+#else
+#define DEBUG_PRINT(fmt, args...) /* Don't do anything in release builds */
+#endif
 
