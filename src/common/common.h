ac7a3e7f4b5cd1c3b61d57ed7a2ce4e11e3b1fa4
Ofri Snir <ofri.snir@gmail.com>
2020-04-12 15:04:46 -0700

    Add common macros
    Added macros useful macros and defines to the common header file

--- src/common/common.h
+++ src/common/common.h
@@ -2,12 +2,29 @@
 #include <stdio.h>
 #include <errno.h>
 #include <string.h>
+#include <unistd.h>
 
 /*** Constants ***/
+/* Value of an invalid file descriptor. */
+#define INVALID_FILE_DESCRIPTOR (-1)
 
 /* Return values. */
 #define C_FAILURE (-1)
 #define C_SUCCESS (0)
+
+#define CLOSE(__fd) \
+do { \
+    if (INVALID_FILE_DESCRIPTOR != (__fd)) { \
+        (void)close(__fd); \
+    } \
+} while (0)
+
+#define HEAPFREE(__buffer) \
+do { \
+    if (NULL != (__buffer)) { \
+        (void)free(__buffer); \
+    } \
+} while (0)
 
 #ifdef DEBUG
     #define DEBUG_PRINT(fmt, args...) fprintf(stderr, "DEBUG: %s:%d:%s(): " fmt "\n", \
